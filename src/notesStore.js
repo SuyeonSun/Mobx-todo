e0cf52b965481fe6e2b5f9a0ab8cb0c0e1234eff
SuyeonSun <suninisy@gmail.com>
2021-12-23 14:43:49 +0900

    first commit

---
+++ src/notesStore.js
@@ -0,0 +1,18 @@
+import {nanoid} from 'nanoid'
+
+export function createNotesStore(){
+  return {
+    notes: [],
+    addNote(text){
+      console.log(text, this.notes)
+      this.notes.push({
+        text, id: nanoid()
+      })
+    },
+    removeNote(id){
+      this.notes = this.notes.filter(note => note.id !== id)
+    }
+  }
+}
+
+// in order to use these globally, we've gotta make the context
